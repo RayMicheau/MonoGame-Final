67d877e009fa81225f0b858dcac6cd3c65a9da2c
Raymond Micheau <raymicheau@yahoo.ca>
2015-03-05 17:41:31 -0600

    Updated Movement

--- MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Game1.cs
+++ MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Game1.cs
@@ -18,13 +18,20 @@ namespace MonoGame_Dynamics_Final_Project
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
-        
+
+        public static Random random;
+
+        Player background;
+        Player playerShip;
+
         public Game1()
             : base()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1620;
+            graphics.PreferredBackBufferHeight = 1014;
+            graphics.ApplyChanges();
         }
 
         /// <summary>
@@ -36,6 +43,7 @@ namespace MonoGame_Dynamics_Final_Project
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            
 
             base.Initialize();
         }
@@ -48,6 +56,18 @@ namespace MonoGame_Dynamics_Final_Project
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            background = new Player(Content.Load<Texture2D>("Images/universe0"),
+                Vector2.Zero,
+                Vector2.Zero,
+                false,
+                4.0f);
+
+            playerShip = new Player(Content.Load<Texture2D>("Images/Commandunit0"),
+                new Vector2(100,100),
+                new Vector2(20,20),
+                true,
+                1.0f);
 
             // TODO: use this.Content to load your game content here
         }
@@ -71,9 +91,58 @@ namespace MonoGame_Dynamics_Final_Project
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            UpdateInput();
+            playerShip.Update(gameTime, GraphicsDevice);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+        }
+        private void UpdateInput()
+        {
+            //set keyPressed to false to start.
+            //if it's still false after all keys have been tested, 
+            //nothing relevant has been pressed
+            //and we should idle
+            bool keyPressed = false;
+            KeyboardState keyState = Keyboard.GetState();
+            GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+
+            if (keyState.IsKeyDown(Keys.Up)
+              || keyState.IsKeyDown(Keys.W)
+              || gamePadState.DPad.Up == ButtonState.Pressed
+              || gamePadState.ThumbSticks.Left.Y > 0)
+            {
+                playerShip.Up();
+                keyPressed = true;
+            }
+            if (keyState.IsKeyDown(Keys.Down)
+              || keyState.IsKeyDown(Keys.S)
+              || gamePadState.DPad.Down == ButtonState.Pressed
+              || gamePadState.ThumbSticks.Left.Y < -0.5f)
+            {
+                playerShip.Down();
+                keyPressed = true;
+            }
+            if (keyState.IsKeyDown(Keys.Left)
+              || keyState.IsKeyDown(Keys.A)
+              || gamePadState.DPad.Left == ButtonState.Pressed
+              || gamePadState.ThumbSticks.Left.X < -0.5f)
+            {
+                playerShip.Left();
+                keyPressed = true;
+            }
+            if (keyState.IsKeyDown(Keys.Right)
+              || keyState.IsKeyDown(Keys.D)
+              || gamePadState.DPad.Right == ButtonState.Pressed
+              || gamePadState.ThumbSticks.Left.X > 0.5f)
+            {
+                playerShip.Right();
+                keyPressed = true;
+            }
+            if (!keyPressed)
+            {
+                playerShip.Idle();
+            }
         }
 
         /// <summary>
@@ -85,6 +154,12 @@ namespace MonoGame_Dynamics_Final_Project
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            background.Draw(spriteBatch);
+            playerShip.Draw(spriteBatch);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Player.cs
+++ MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Player.cs
@@ -57,6 +57,8 @@ namespace MonoGame_Dynamics_Final_Project
         public bool SetOrigin { get; set; }
         public float Scale { get; set; }
 
+        protected SpriteEffects Spriteeffect { get; set; }
+
         public bool Alive { get; set; }
 
         #endregion
@@ -73,6 +75,109 @@ namespace MonoGame_Dynamics_Final_Project
             }
         }
 
-        
+        public Player(Texture2D textureImage, Vector2 position, Vector2 velocity, bool setOrig, float scale)
+        {
+            Position = position;
+            TextureImage = textureImage;
+            InitialVelocity = velocity;
+            Velocity = velocity;
+            SetOrigin = setOrig;
+            if (SetOrigin)
+            {
+                SpriteOrigin = new Vector2(TextureImage.Width / 2, TextureImage.Height / 2);
+            }
+            Scale = scale;
+            Alive = true;
+        }
+
+        //Update that doesn't keep sprites onscreen
+        public virtual void Update(GameTime gameTime)
+        {
+            if (Alive)
+            {
+                //Time between the frames
+                float timeLapse = (float)(gameTime.ElapsedGameTime.TotalSeconds);
+
+                //Move the sprite
+                position += Velocity * timeLapse;
+            }
+        }
+
+        public virtual void Update(GameTime gameTime, GraphicsDevice Device)
+        {
+            if (Alive)
+            {
+                //Keep the sprite onscreen
+                Update(gameTime);
+
+                if (Position.X > Device.Viewport.Width - SpriteOrigin.X * Scale)
+                {
+                    position.X = 0 + SpriteOrigin.X * Scale;
+                }
+                else if (Position.X < SpriteOrigin.X * Scale)
+                {
+                    position.X = Device.Viewport.Width - SpriteOrigin.X * Scale;
+                }
+
+                if (Position.Y > Device.Viewport.Height - SpriteOrigin.Y * Scale)
+                {
+                    position.Y = Device.Viewport.Height - SpriteOrigin.Y * Scale;
+                    velocity.Y = -Velocity.Y;
+                }
+                else if (Position.Y < SpriteOrigin.Y * Scale)
+                {
+                    position.Y = SpriteOrigin.Y * Scale;
+                    velocity.Y = -Velocity.Y;
+                }
+            }
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (Alive)
+            {
+                spriteBatch.Draw(TextureImage, 
+                    position, 
+                    null, 
+                    Microsoft.Xna.Framework.Color.White, 
+                    0f, 
+                    SpriteOrigin, 
+                    Scale, 
+                    Spriteeffect,
+                    0);
+            }
+        }
+
+        public bool CollisionSprite(Player sprite)
+        {
+            return CollisionRectangle.Intersects(sprite.CollisionRectangle);
+        }
+
+        public void Up()
+        {
+            velocity.Y -= InitialVelocity.Y;;
+        }
+
+        public void Down()
+        {
+            velocity.Y += InitialVelocity.Y;;
+        }
+
+        public virtual void Right() 
+        {
+            velocity.X += InitialVelocity.X;
+            position.Y += initialVelocity.Y / 3;
+        }
+
+        public virtual void Left()
+        {
+            velocity.X -= InitialVelocity.X;
+            position.Y += initialVelocity.Y / 3;
+        }
+        public virtual void Idle()
+        {
+            Velocity = Velocity * .98f;
+            position.Y += initialVelocity.Y / 3;
+
+        }
     }
 }
