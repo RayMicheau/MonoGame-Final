0363b3ffd6b2c96c3144c0be87420d7975e36428
Raymond Micheau <raymicheau@yahoo.ca>
2015-03-05 15:51:49 -0600

    Adding Project Files
    Added all the project files for the semester two final project.

---
+++ MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Player.cs
@@ -0,0 +1,78 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+#endregion
+
+namespace MonoGame_Dynamics_Final_Project
+{
+    class Player
+    {
+        
+        //IDEAL CLASS SET UP
+        //Set up all player variables to be used
+        //Create Player constructor
+        //An Update function for keeping track of gametime
+        //Another update for checking screenwidths
+        //And a draw method
+        //As well as collision and movement methods.
+
+        #region Variables
+
+        protected Vector2 position;
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        protected Vector2 spriteOrigin;
+        public Vector2 SpriteOrigin
+        {
+            get { return spriteOrigin; }
+            set { spriteOrigin = value; }
+        }
+
+        protected Vector2 velocity;
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+        
+        protected Vector2 initialVelocity;
+        public Vector2 InitialVelocity
+        {
+            get { return initialVelocity; }
+            set { initialVelocity = value; }
+        }
+
+        public bool SetOrigin { get; set; }
+        public float Scale { get; set; }
+
+        public bool Alive { get; set; }
+
+        #endregion
+
+        //Texture object and a collision rectangle
+        public Texture2D TextureImage { get; set; }
+
+        public virtual Rectangle CollisionRectangle
+        {
+            get
+            {
+                return new Rectangle((int)(position.X - SpriteOrigin.X * Scale), (int)(position.Y - SpriteOrigin.Y * Scale),
+                   Convert.ToInt32(TextureImage.Width * Scale), Convert.ToInt32(TextureImage.Height * Scale));
+            }
+        }
+
+        
+    }
+}
