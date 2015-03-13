e1840bf1b8cdb5b073fa026f0ffbf36e2905355f
hau5tastic <hau5tastic@gmail.com>
2015-03-13 12:59:57 -0400

    added hello comment

--- MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Game1.cs
+++ MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Game1.cs
@@ -56,7 +56,7 @@ namespace MonoGame_Dynamics_Final_Project
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            // helo
             Song song = Content.Load<Song>("TellMe");
             MediaPlayer.Play(song);
 
