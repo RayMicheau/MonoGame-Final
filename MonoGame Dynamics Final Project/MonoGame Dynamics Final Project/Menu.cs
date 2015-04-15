961d228cda80cf3feec22cb2661873e768d797ab
Raymond Micheau <raymicheau@yahoo.ca>
2015-04-15 15:36:40 -0400

    Menu.cs
    Menu class added, going to add a menu shortly.

---
+++ MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Menu.cs
@@ -0,0 +1,21 @@
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using MonoGame_Dynamics_Final_Project.Sprites;
+using MonoGame_Dynamics_Final_Project.Weapons;
+#endregion
+
+namespace MonoGame_Dynamics_Final_Project
+{
+    public class Menu
+    {
+ 
+    }
+}
