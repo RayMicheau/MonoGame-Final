96d7c567e060e771937835193dccc9f6f532c5f6
Raymond Micheau <raymicheau@yahoo.ca>
2015-04-25 15:21:44 -0400

    PowerUp addition
    PowerUp.CS added

---
+++ MonoGame Dynamics Final Project/MonoGame Dynamics Final Project/Sprites/PowerUp.cs
@@ -0,0 +1,51 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using MonoGame_Dynamics_Final_Project.Sprites;
+using MonoGame_Dynamics_Final_Project;
+#endregion
+
+namespace MonoGame_Dynamics_Final_Project.Sprites
+{
+    public enum PowerUps{
+        Null,
+        AtkSpdUp,
+        MoveSpdUp,
+        Shield
+    }
+
+    class PowerUp : Enemy
+    {
+        float dTime;
+        public bool removeFromScreen = false;
+        PowerUps powerUps = PowerUps.Null;
+        public PowerUp(int width, int height, Texture2D textureImg, GraphicsDevice Device, int spotInFormation, string formationType, float scale, float damage, float health, PowerUps PowerUp)
+            : base(width, height, textureImg, Device, spotInFormation, formationType, scale, damage, health)
+        {
+            if(powerUps != PowerUps.Null){
+                switch(powerUps){
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void Update(GameTime gameTime, GraphicsDevice graphicsDevice)
+        {
+            dTime = (float)gameTime.ElapsedGameTime.TotalMilliseconds /1000.0f;
+            position.Y = Velocity.Y * dTime;
+            if (Position.Y >= graphicsDevice.Viewport.Height)
+            {
+                removeFromScreen = true;
+            }
+        }
+    }
+}
